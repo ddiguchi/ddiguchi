233728f18f56b3aa5f66a7b23549d98c8a0ca214
ddiguchi <85557896+ddiguchi@users.noreply.github.com>
2021-06-15 23:14:48 +0900

    Create list.java

---
+++ list.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<>();
+		 
+        list1.add("テトリス");
+        list1.add("ぷよぷよ");
+        list1.add("コラムス");
+        list1.add("パネルでポン");
+        
+        System.out.println(list1);
+	}
+}
