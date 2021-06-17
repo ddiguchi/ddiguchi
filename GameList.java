d29485933199945b8a22ee922b07a26756f1f10e
ddiguchi <85557896+ddiguchi@users.noreply.github.com>
2021-06-18 00:28:57 +0900

    Update and rename list.java to GameList.java

---
+++ GameList.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameList {
+	public static void main(String[] args) {
+		List<String> list1 = new ArrayList<>();
+		 list1.add("テトリス");
+		 list1.add("ぷよぷよ");
+		 list1.add("コラムス");
+		 list1.add("パネルでポン");
+		 System.out.println(list1);
+	}
+}
